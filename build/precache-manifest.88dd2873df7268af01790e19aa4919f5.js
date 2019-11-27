8e083a02e2e4822a169c66f7c77924091bb1fdc5
Chenbozhiaaaaaa <45990434+Chenbozhiaaaaaa@users.noreply.github.com>
2019-11-27 14:36:11 +0800

    1

---
+++ build/precache-manifest.88dd2873df7268af01790e19aa4919f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c812bcdc1b9172124fafcb9b1c030f29",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a2b48d365a544e18518",
+    "url": "./static/css/2.0f87f62a.chunk.css"
+  },
+  {
+    "revision": "376712ed2515b0cb30a2",
+    "url": "./static/css/main.0a802d11.chunk.css"
+  },
+  {
+    "revision": "8a2b48d365a544e18518",
+    "url": "./static/js/2.fc22b20a.chunk.js"
+  },
+  {
+    "revision": "376712ed2515b0cb30a2",
+    "url": "./static/js/main.92c3caea.chunk.js"
+  },
+  {
+    "revision": "c466be1d615e0ab1f35c",
+    "url": "./static/js/runtime-main.7230e7d2.js"
+  }
+]);
