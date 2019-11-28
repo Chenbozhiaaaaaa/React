0405db7a142a986158b18aa2e286bc9435b7482c
Chenbozhiaaaaaa <45990434+Chenbozhiaaaaaa@users.noreply.github.com>
2019-11-28 14:34:07 +0800

    1
    调换天气接口

--- src/compontents/Header/index.js
+++ src/compontents/Header/index.js
@@ -1,43 +1,53 @@
 import React, { Component } from 'react'
-import { Row,Col } from 'antd'
+import { Row, Col } from 'antd'
 import './index.css'
 import Util from '../../utils/utils'
 import axios from '../../axios/index'
+import Axios from 'axios'
 export default class Header extends Component {
     componentWillMount() {
         this.setState({
             userName: '我的天呐'
         })
-        setInterval(()=>{
+        setInterval(() => {
             let sysTime = Util.formateDate(new Date().getTime())
             this.setState({
                 sysTime
             })
-        },1000)
+        }, 1000)
         this.getWeatherAPIData()
     }
     // 防止内存泄漏
-    componentWillUnmount(){
+    componentWillUnmount() {
         this.setState = (state, callback) => {
             return;
-          };
+        };
     }
-    getWeatherAPIData(){
+    getWeatherAPIData() {
         let city = '北京'
-        axios.jsonp({
-            url:`http://wthrcdn.etouch.cn/weather_mini?city=${city}`
-        }).then(res=>{
-            if(res.status=='1000'){
+        // axios.jsonp({
+        //     url:`http://wthrcdn.etouch.cn/weather_mini?city=${city}`
+        // }).then(res=>{
+        //     if(res.status=='1000'){
+        //         this.setState({
+        //             weatherDatas:res.data.forecast[0].type,
+        //             weatherDatash:(res.data.forecast[0].high).substring(2),
+        //             weatherDatasl:(res.data.forecast[0].low).substring(2)
+        //         })
+        //     }
+        // })
+        // 这里单独引入了一个axios 因为天气接口是第三方的 封装的jsonp不能满足格式
+        Axios.get(`https://www.tianqiapi.com/api/?appid=58984568&appsecret=6i21A53n&version=v6&city=%E5%8C%97%E4%BA%AC`).then(res => {
+            let meta = res.data
+            console.log(meta);
+            if (res.status == '200') {
                 this.setState({
-                    weatherDatas:res.data.forecast[0].type,
-                    weatherDatash:(res.data.forecast[0].high).substring(2),
-                    weatherDatasl:(res.data.forecast[0].low).substring(2)
+                    weatherCity:meta.city,
+                    weatherDatas: meta.wea,
+                    weatherDatash: meta.tem1,
+                    weatherDatasl: meta.tem2
                 })
-            }else{
-                
             }
-        }).catch(err=>{
-
         })
     }
     render() {
@@ -55,9 +65,10 @@ export default class Header extends Component {
                     </Col>
                     <Col span={20} className='weather'>
                         <span className="date">{this.state.sysTime}</span>
+                        <span className="date">{this.state.weatherCity}</span>
                         <span className='weather-detail'>{this.state.weatherDatas} </span>
-                        <span className='weather-detail'> 气温:{this.state.weatherDatash}/{this.state.weatherDatasl}</span>
-                     
+                        <span className='weather-detail'> 气温:{this.state.weatherDatash}&#176;/{this.state.weatherDatasl}&#176;</span>
+
                     </Col>
                 </Row>
             </div>
